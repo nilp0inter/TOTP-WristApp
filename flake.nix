47e6fa962d4a52537d8764fd507ad58c36fcc739
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2024-02-01 20:55:30 +0100

    chore: add a 6800 emulator to the toolkit

--- flake.nix
+++ flake.nix
@@ -8,13 +8,24 @@
     };
   };
 
-  outputs = { self, nixpkgs, assembler }: {
+  outputs = { self, nixpkgs, assembler }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    WIN6811IDE = let
+      src = pkgs.fetchzip {
+        url = "http://www.activesky.com/6811IDE.zip";
+        hash = "sha256-AQMvn3Lb2IPUCNNzXMevILj/2o2bBdhfmPflsvHhgXc=";
+        stripRoot = false;
+      };
+    in pkgs.writeShellScriptBin "6811ide" ''
+      ${pkgs.wine}/bin/wine ${src}/SDK6811.exe
+    '';
+  in {
+    packages.x86_64-linux.WIN6811IDE = WIN6811IDE;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.WIN6811IDE;
 
-    devShells.x86_64-linux.default = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in pkgs.mkShell {
+    devShells.x86_64-linux.default = pkgs.mkShell {
       shellHook = ''
-        export PATH="${assembler}:$PATH"
+        export PATH="${assembler}:${WIN6811IDE}/bin:$PATH"
       '';
     };
   };
